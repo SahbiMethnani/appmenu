976810feb3ffa71a4b3a0548fcc500cafc0a2a0d
SahbiMethnani <sahbimethnani3@gmail.com>
2026-01-31 15:42:18 +0100

    test 11

--- src/test/java/com/example/appmenu/repository/PlatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.appmenu.repository;
-
-import com.example.appmenu.entity.Plat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatRepository extends JpaRepository<Plat, Long> {}
--- src/test/java/com/example/appmenu/repository/RestaurantConfigRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.appmenu.repository;
-
-import com.example.appmenu.entity.RestaurantConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantConfigRepository extends JpaRepository<RestaurantConfig, Long> {}
